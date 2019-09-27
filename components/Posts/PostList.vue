549ec7f7b0eceff59a08cf97c5936275119badca
zero4897hn <zero4897hncool@gmail.com>
2019-09-27 15:57:03 +0700

    lesson 3.13 done

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="featured-posts">
-        <PostPreview id="1" thumbnail="https://cdn.cjr.org/wp-content/uploads/2019/07/AdobeStock_165953143-1300x500.jpeg" title="Hello there!" previewText="This is my first post!" />
-        <PostPreview id="2" thumbnail="https://cdn.cjr.org/wp-content/uploads/2019/07/AdobeStock_165953143-1300x500.jpeg" title="Hello there!" previewText="This is my first post!" />
-        <PostPreview id="3" thumbnail="https://cdn.cjr.org/wp-content/uploads/2019/07/AdobeStock_165953143-1300x500.jpeg" title="Hello there!" previewText="This is my first post!" />
+        <PostPreview id="1" :is-admin="isAdmin" thumbnail="https://cdn.cjr.org/wp-content/uploads/2019/07/AdobeStock_165953143-1300x500.jpeg" title="Hello there!" previewText="This is my first post!" />
+        <PostPreview id="2" :is-admin="isAdmin" thumbnail="https://cdn.cjr.org/wp-content/uploads/2019/07/AdobeStock_165953143-1300x500.jpeg" title="Hello there!" previewText="This is my first post!" />
+        <PostPreview id="3" :is-admin="isAdmin" thumbnail="https://cdn.cjr.org/wp-content/uploads/2019/07/AdobeStock_165953143-1300x500.jpeg" title="Hello there!" previewText="This is my first post!" />
     </section>
 </template>
 
@@ -10,6 +10,12 @@
 import PostPreview from '@/components/Posts/PostPreview';
 
 export default {
+	props: {
+		isAdmin: {
+			type: Boolean,
+			default: false
+		}
+	},
 	components: {
 		PostPreview
 	}
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post='loadedPost' />
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm';
+
+export default {
+    layout: 'admin',
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Kobayashi Zero',
+                title: 'My awesome post',
+                content: 'Super amazing, thanks for that!',
+                thumbnailLink: 'https://cdn.cjr.org/wp-content/uploads/2019/07/AdobeStock_165953143-1300x500.jpeg'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.update-form {
+    width: 90%;
+    margin: 20px auto;
+}
+@media (min-width: 768px) {
+    .update-form {
+        width: 500px;
+    }
+}
+</style>
